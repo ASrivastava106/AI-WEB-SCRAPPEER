24ff699338303125c564542ce17780a5c58c3bb2
Aishwary Srivastava <125578182+ASrivastava106@users.noreply.github.com>
2025-03-13 09:24:27 +0530

    Add files via upload
    Developed an AI-powered web scraping tool using Python, Selenium, BeautifulSoup, LangChain (Ollama LLM), and Streamlit to automate data extraction and processing.

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from scrape import (scrape_website,
+                    split_dom_content,
+                    clean_body_content,
+                    extract_body_content
+                    )
+
+st.title("AI Web Scraper")
+url = st.text_input("Enter Website URL")
+
+# Step 1: Scrape the Website
+if st.button("Scrape Website"):
+    if url:
+        st.write("Scraping the website...")
+
+        # Scrape the website
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
+
+        # Store the DOM content in Streamlit session state
+        st.session_state.dom_content = cleaned_content
+
+        # Display the DOM content in an expandable text box
+        with st.expander("View DOM Content"):
+            st.text_area("DOM Content", cleaned_content, height=300)
+
+
+
